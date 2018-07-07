69f820e77a58fbe013fe5b77790ba5a32bb4b1a0
Weiying Yi <tony-yi@Tonys-MBA.local>
2018-07-07 14:10:29 +0800

    testing

--- src/display.py
+++ src/display.py
@@ -1,29 +1,3 @@
-from lib import *
+from lib import test
 
-# create a window
-window = tkinter.Tk()
-
-# add title to the window
-window.title('pi_display')
-
-# make the window full screen and quit when pressing 'Esc'
-window.attributes('-fullscreen', True)
-window.bind('<Escape>',lambda e: window.destroy())
-
-# load a image uisng OpenCV
-cv_img = cv2.imread('../img/angry_cat.jpg')
-img_h, img_w, img_c = cv_img.shape
-print('img_h: %d\timg_w: %d\timg_c: %d\t' % (img_h, img_w, img_c))
-
-# create a canvas than can fit the above image
-canvas = tkinter.Canvas(window, width=img_w, height=img_h)
-canvas.pack()
-
-# use Pillow to convert the Numpy ndarray to a PhotoImage
-photo = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(cv_img))
-
-# add a PhotoImage to the canvas
-canvas.create_image(0, 0, image=photo, anchor=tkinter.NW)
-
-# run the window loop
-window.mainloop()
+test()
