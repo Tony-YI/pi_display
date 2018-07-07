752c7474d305aa3aff59fc744a0361541948817a
Weiying Yi <tony-yi@Tonys-MBA.local>
2018-07-07 18:35:31 +0800

    testing

--- src/lib.py
+++ src/lib.py
@@ -5,58 +5,78 @@ def getFilenames(dir_name):
 	filenames = os.listdir(dir_name)
 	return filenames
 
-def createFullScreenWindow():
-	return 0
-
 class App:
-	def __init__(self, img_path, window, win_title='pi_display', win_bg='black',debug=True):
-		# create a window and add a title to it
-		self.window = window
-		self.window.title(win_title)
+	def __init__(self, img_path, window, win_title='pi_display', win_bg='black', debug=True):
+		self.window, self.win_w, self.win_h = \
+			self.createWindow(window, win_title, win_bg, debug)
+		
+		self.cv_img, self.img_w, self.img_h = \
+			self.loadImg(img_path, debug)
 
-		# set the backgournd color to black
-		self.window.configure(background=win_bg)
+		self.cv_img, self.img_w, self.img_h = \
+			self.resizeImg(self.cv_img, self.img_w, self.img_h, self.win_w, self.win_h, debug)
 
-		# make the window full screen and quit when pressing 'Esc'
-		self.window.attributes('-fullscreen', True)
-		self.window.bind('<Escape>',lambda e: self.window.destroy())
-		self.win_w = self.window.winfo_screenwidth()
-		self.win_h = self.window.winfo_screenheight()
-		if debug:
-			print('win_w: %d\twin_h: %d\t' % (self.win_w, self.win_h))
+		self.photo = \
+			self.convertCVImgToPILImage(self.cv_img, debug)
 
-		# load a image uisng OpenCV
-		self.cv_img = cv2.imread(img_path)
-		self.cv_img = cv2.cvtColor(self.cv_img, cv2.COLOR_BGR2RGB) # convet BGR to RGB
-		self.img_h, self.img_w, self.img_c = self.cv_img.shape
-		if debug:
-			print('ori_img_w: %d\tori_img_h: %d\tori_img_c: %d' 
-				% (self.img_w, self.img_h, self.img_c))
-
-		# resize image to fit the window
-		if self.img_w > self.win_w or self.img_h > self.win_h:
-			self.ratio = min(self.win_w/self.img_w, self.win_h/self.img_h)
-			self.img_w = int(self.img_w * self.ratio)
-			self.img_h = int(self.img_h * self.ratio)
-			self.cv_img = cv2.resize(self.cv_img, (self.img_w, self.img_h))
-			if debug:
-				print('ratio: %f\tnew_img_w: %d\tnew_img_h: %d' 
-					% (self.ratio, self.img_w, self.img_h))
-
-		# use Pillow to convert the Numpy ndarray to a PhotoImage
-		self.photo = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(self.cv_img))
-
-		# create a canvas than can fit the above image
-		self.canvas = tkinter.Canvas(self.window, 
-			width=self.img_w, height=self.img_h, relief='flat', highlightthickness=0)
-		self.canvas.pack()
-		self.canvas.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER) # center the canvas
-
-		# add a PhotoImage to the canvas
-		self.canvas.create_image(self.img_w/2, self.img_h/2, 
-			image=self.photo, anchor=tkinter.CENTER) # center the image inside canvas
-
-
+		self.canvas = \
+			self.createCanvas(self.window, self.photo, self.img_w, self.img_h, debug)
 
 		# run the window loop
 		self.window.mainloop()
+
+	def createWindow(self, window, win_title, win_bg, debug):
+		# create a window and add a title to it
+		window.title(win_title)
+
+		# set the backgournd color to black
+		window.configure(background=win_bg)
+
+		# make the window full screen and quit when pressing 'Esc'
+		window.attributes('-fullscreen', True)
+		window.bind('<Escape>',lambda e: window.destroy())
+		win_w = window.winfo_screenwidth()
+		win_h = window.winfo_screenheight()
+		if debug:
+			print('win_w: %d\twin_h: %d\t' % (win_w, win_h))
+		return window, win_w, win_h
+
+
+	def loadImg(self, img_path, debug):
+		# load a image uisng OpenCV
+		cv_img = cv2.imread(img_path)
+		cv_img = cv2.cvtColor(cv_img, cv2.COLOR_BGR2RGB) # convet BGR to RGB
+		img_h, img_w, img_c = cv_img.shape
+		if debug:
+			print('ori_img_w: %d\tori_img_h: %d\tori_img_c: %d' 
+				% (img_w, img_h, img_c))
+		return cv_img, img_w, img_h
+
+	def resizeImg(self, cv_img, img_w, img_h, win_w, win_h, debug):
+		# resize image to fit the window
+		if img_w > win_w or img_h > win_h:
+			ratio = min(win_w/img_w, win_h/img_h)
+			img_w = int(img_w * ratio)
+			img_h = int(img_h * ratio)
+			cv_img = cv2.resize(cv_img, (img_w, img_h))
+			if debug:
+				print('ratio: %f\tnew_img_w: %d\tnew_img_h: %d' 
+					% (ratio, img_w, img_h))
+		return cv_img, img_w, img_h
+
+	def convertCVImgToPILImage(self, cv_img, debug):
+		# use Pillow to convert the Numpy ndarray to a PhotoImage
+		photo = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(cv_img))
+		return photo
+
+	def createCanvas(self, window, photo, img_w, img_h, debug):
+		# create a canvas than can fit the above image
+		canvas = tkinter.Canvas(window, 
+			width=img_w, height=img_h, relief='flat', highlightthickness=0)
+		canvas.pack()
+		canvas.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER) # center the canvas
+
+		# add a PhotoImage to the canvas
+		canvas.create_image(img_w/2, img_h/2, 
+			image=photo, anchor=tkinter.CENTER) # center the image inside canvas
+		return canvas
